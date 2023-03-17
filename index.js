e1fd4e2855a2f04acb3c7d90f9e6acf63748b699
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-03-17 21:36:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Create a simple todo list that has the following functionalities
+
+/**
+ * add a new item to the todo list
+ * remove an item in the todo list
+ * edit an item in the todo list
+    Some further requirements:
+    - the input field must be reset when the add button is clicked on 
+    - the todo list must show the most recent first
+    - prevent empty values from being added to the list
+    - the todo list should be stored using localStorage
+ */
+
+const newTaskInput = document.getElementById('new-task');
+let todoList = [];
+
+function addItem () {
+}
+
+function editItem (item) {
+
+}
+
+function deleteItem (item) {
+
+}
+
+
