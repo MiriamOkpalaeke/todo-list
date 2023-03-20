195d9fce7668f23848407ab6901f678d3f1c3d01
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-03-20 21:03:48 +0100

    still working on js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
+
 let input = document.querySelector("#new-task");
 let addBtn = document.querySelector("#add-button");
 let tasks = document.querySelector(".tasks");
+//let deleteBtn = document.querySelector('#delete-button');
 
-//add btn disabled
-
-input.addEventListener("keyup", () => {
-  if (input.value.trim() != 0) {
-    addBtn.classList.add("active");
-  } else {
-    addBtn.classList.remove("active");
-  }
-});
 
 //adding new task
 addBtn.addEventListener("click", () => {
@@ -18,6 +11,7 @@ addBtn.addEventListener("click", () => {
     let newItem = document.createElement("div");
     newItem.classList.add("item");
     newItem.innerHTML = `
+    <div id="item">
     <p> ${input.value} </p>
             <div class="item-btn">
               <button id="edit-button">Edit</button>
@@ -33,14 +27,9 @@ addBtn.addEventListener("click", () => {
 
 //delete item
 tasks.addEventListener("click", (e) => {
-   if (e.target.classList.contains("delete-button")){
-      e.target.parentElement.parentElement.remove();
+   if (e.target.classList.contains("button")){
+      e.target.parentElement.remove();
    }
 })
 
 
-tasks.addEventListener("click", (e) => {
-   if (e.target.classList.contains("Delete")){
-      e.target.parentElement.parentElement.remove();
-   }
-})
